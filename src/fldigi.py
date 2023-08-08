c3eec3d26e4b2c5ecb2cc37c34bd221dc99d4a15
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-08-07 20:35:59 -0700

    first commit

---
+++ src/fldigi.py
@@ -0,0 +1,104 @@
+import pyfldigi
+import time
+
+msg_recived = False
+
+
+#FLDIGI START AND INITIALIZE HERE
+
+fldigi = pyfldigi.Client()
+
+
+#GETTERS AND SETTERS HERE
+
+
+def get_modem_name():
+
+    return fldigi.modem.name
+
+def get_modem_carrier():
+
+    return fldigi.modem.name
+
+def get_rig_frequency():
+
+    return fldigi.rig.frequency
+
+def get_rig_mode():
+
+    return fldigi.rig.frequency
+
+def get_text():
+
+    print(fldigi.text.get_tx_data())
+
+def set_modem(name, carrier):
+
+    fldigi.modem.name = name
+
+    fldigi.modem.name = name
+
+    fldigi.modem.carrier = carrier
+
+def set_frequency(frequency, mode):
+    fldigi.rig.frequency = frequency
+    fldigi.rig.mode = mode
+
+
+def send_tx(json):
+
+    fldigi.text.clear_rx()
+    fldigi.text.clear_tx()
+    fldigi.text.add_tx(json)
+    fldigi.main.send(json, block=True, timeout=500)
+
+    print("Text Sent..")
+
+
+def get_rx():
+
+    
+    rx_text = fldigi.text.get_rx_data()
+
+    return rx_text
+
+
+#LOGIC GOES UNDER HERE
+
+def radio_setup(mode, c_freq):
+
+    if get_modem_name != mode or get_modem_carrier != c_freq:
+
+        set_modem(mode, c_freq)
+
+    if get_rig_frequency != 7071000.0 or get_rig_mode != 'USB':
+
+        set_frequency(7071000.0, 'USB')
+
+while True:
+
+    if get_rx():
+
+        decoded = (get_rx())
+        decoded_string = decoded.decode()
+        print (decoded_string)
+
+        msg_recived = True
+        
+
+
+    if msg_recived == True:
+
+
+        print("Received found data. Yay!")
+
+        print(get_rx())
+
+        time.sleep(1)
+
+
+    else:
+      
+        print("Nope, continuing to listen")
+        time.sleep(1)
+
---
+++ src/main.py
@@ -0,0 +1,22 @@
+import fldigi as fl
+
+
+# ------- FLDigi Calls Done ----------#
+
+fl.radio_setup("BPSK63", 1000)
+
+print('Setting up radio...')
+
+text = input('Please enter the text to transmit:\n')
+
+confirm_tx = input("Do you want to transmit? Y/N\n")
+
+if confirm_tx.upper() == 'Y':
+
+    fl.send_tx("<MYCALL> " + text)
+
+else:
+    print("You chose not to transmit")
+
+# ------- BEGIN NOSTR ------------ #
+
