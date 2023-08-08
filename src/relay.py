78bc147b2a947eea50b361de9b07629dc74a9cce
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-08-08 12:13:47 -0700

    Working Relay Connect and request

--- src/relay.py
+++ src/relay.py
@@ -4,15 +4,17 @@ ws = websocket.WebSocket()
 ws.connect("wss://relay.snort.social")
 
 relay_need = '"REQ"'
-kind = '"kind":[0,2]'
-limit = '"limit":20'
-subscription = '"ec3e6238-5ef3-4162-899c-a58e82586a4b"'
+kind = '"kinds":[1]'
+limit = '"limit":5'
+subscription = '"ec3e6238-5ef3-4162-899c-a58825845a4c"'
 
 request = '['+relay_need+', '+ subscription+', {'+ kind +', '+ limit + '}]'
 
 while True:
 
     ws.send(request)
-    print(ws.recv())
-
+    event = ws.recv()
+    event = event[50:]
+    event_json = json.loads(event)
+    print(event_json)
 
