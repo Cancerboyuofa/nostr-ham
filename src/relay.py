729d8133adf36708fb4163c1a1e9095c81d81c7e
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-08-08 16:01:26 -0700

    Added get last 10 of my own notes for testing only.

--- src/relay.py
+++ src/relay.py
@@ -1,40 +1,48 @@
 import websocket
 import json
 
-
+    
 
 if __name__ == "__main__":
 
     
+    # Sockets Setup
+    
     ws = websocket.WebSocket()
     ws.connect("wss://nos.lol")
 
+    # Global Setup
+
     relay_req = '"REQ"'
-    kind = '"kinds":[1]'
-    limit = '"limit":10'
+    kind = '[1]'
+    limit = 20
     subscription = '"6393049621547051"'
     author = '"authors":["e3aefda887252a72cee3578d33b2dcd90e9fe53b8bed6347ef5e26f74211adbb"]'
 
+    request = '['+relay_req+', '+ subscription+', {"kinds":'+ kind +', "limit":'+ str(limit) + ', ' +author +'}]'
 
-    request = '['+relay_req+', '+ subscription+', {'+ kind +', '+ limit + ', ' +author +'}]'
 
-    i = 0
+    ws.send(request)
+    
 
     print("Here is the content of my last 10 notes:\n")
 
+    i = 0
+    n = 0
+
     while i <= 10:
         
-        ws.send(request)
         event = ws.recv()
 
         json_start = event.rfind('{')
   
         clean_event = event[json_start:]
 
-        clean_event = clean_event.rstrip(clean_event[-1])
+        stripped_event = clean_event.rstrip(clean_event[-1])
 
-        event_json = json.loads(clean_event)
+        event_json = json.loads(stripped_event)
 
-        print(event_json["content"],"\n")
+        print(n,event_json["content"],"\n")
  
-        i = i + 1
+        i += 1
+        n += 1
