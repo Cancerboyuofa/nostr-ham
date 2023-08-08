5e19e99ce7555bdb683195da37440d354b1bed83
Cancerboyuofa <93504221+Cancerboyuofa@users.noreply.github.com>
2023-08-07 21:08:42 -0700

    Update key.py

--- src/key.py
+++ src/key.py
@@ -1,7 +1,28 @@
+from dotenv import load_dotenv
 from pynostr.key import PrivateKey
+from os.path import join, dirname
+import os
 
-private_key = PrivateKey()
+# Check env for existing keys, if null, then create
 
-public_key = private_key.public_key
-print(f"Private key: {private_key.bech32()}")
-print(f"Public key: {public_key.bech32()}")
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+pvt_key = os.environ.get('PRIVATE_KEY')
+pub_key = os.environ.get('PUBLIC_KEY')
+
+def create_keys():
+
+    private_key = PrivateKey()
+    private_key_hex = private_key.bech32()
+
+
+    public_key = private_key.public_key
+    public_key_hex = public_key.bech32()
+
+    return [private_key_hex, public_key_hex]
+
+new_keys = create_keys()
+
+print(new_keys[0])
+print(new_keys[1])
